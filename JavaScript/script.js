453c12159ffea079ab6272641a057abf4989ad21
RANV33R07 <ranveer072002@gmail.com>
2024-03-11 21:57:20 +0530

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,8 +1,11 @@
-var obj = {
-    name : function(){
-        console.log(this.marks);
-    },
-    marks : 100,
-    rollno : 19,
-};
-obj.name();
+
+var username;
+function info(username , rollno){
+    this.username = username;
+    this.rollno =  rollno;
+
+}
+info.prototype.printMyUsername = function(){
+    console.log(this.username);}
+var human1 = new info("ranveer" , 19);
+var human2 = new info("abhiram",1); 
